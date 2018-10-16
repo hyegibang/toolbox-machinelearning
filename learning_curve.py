d62e62b2a8e7819155b81ca34dd147a60f042cf1
hyegibang <31522873+hyegibang@users.noreply.github.com>
2018-10-15 23:53:52 -0400

    upload

---
+++ learning_curve.py
@@ -0,0 +1,65 @@
+"""Explore learning curves for classification of handwritten digits"""
+
+import matplotlib.pyplot as plt
+import numpy
+from sklearn.datasets import *
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+
+def display_digits():
+    """Read in the 8x8 pictures of numbers and display 10 of them."""
+    digits = load_digits()
+    print(digits.DESCR)
+    fig = plt.figure()
+    for i in range(10):
+        subplot = fig.add_subplot(5, 2, i + 1)
+        subplot.matshow(numpy.reshape(digits.data[i], (8, 8)), cmap='gray')
+
+    plt.show()
+
+
+
+def train_model():
+    """Train a model on pictures of digits.
+    Read in 8x8 pictures of numbers and evaluate the accuracy of the model
+    when different percentages of the data are used as training data. This
+    y_size plots the average accuracy of the model as a function of the percent
+    of data used to train it.
+    """
+    data = load_digits()
+    num_trials = 500
+    train_percentages = list(range(5, 95, 5))
+    test_accuracies = numpy.zeros(len(train_percentages))
+
+    # train models with training percentages between 5 and 90 (see
+    # train_percentages) and evaluate the resultant accuracy for each.
+    # You should repeat each training percentage num_trials times to smooth out
+    # variability.
+    # For consistency with the previous example use
+    # model = LogisticRegression(C=10**-10) for your learner
+
+    j = 0
+    for i in train_percentages:
+        accuracy_total = 0
+        for trial in range(num_trials):
+            X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=i)
+            model = LogisticRegression(C=10**-10)
+            model.fit(X_train, y_train)
+            accuracy_total += model.score(X_test, y_test)
+        test_accuracies[j] = accuracy_total / num_trials
+        j += 1
+
+        print("Train accuracy %f" %model.score(X_train, y_train))
+        print("Test accuracy %f"%model.score(X_test, y_test))
+
+    fig = plt.figure()
+    plt.plot(train_percentages, test_accuracies)
+    plt.xlabel('Percentage of Data Used for Training')
+    plt.ylabel('Accuracy on Test Set')
+    plt.show()
+
+if __name__ == "__main__":
+    # Feel free to comment/uncomment as needed
+    # display_digits()
+    train_model()
